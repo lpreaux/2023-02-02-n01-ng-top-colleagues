dd0290620da72487be745406a396ef3757acc610
lucas PREAUX <lucas.preau@gmail.com>
2023-02-08 15:31:55 +0100

    feat(create-colleague-form): envoie du nouveau collègue à l'api

--- src/app/shared/components/create-colleague-forms/create-colleague-forms.component.ts
+++ src/app/shared/components/create-colleague-forms/create-colleague-forms.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {ColleagueApi} from "../../../models/colleague-api";
+import {ColleagueService} from "../../../providers/colleague.service";
 
 @Component({
   selector: 'tc-create-colleague-forms',
@@ -7,13 +8,17 @@ import {ColleagueApi} from "../../../models/colleague-api";
   styleUrls: ['./create-colleague-forms.component.scss']
 })
 export class CreateColleagueFormsComponent {
-  colleague: ColleagueApi = {
+  colleague: ColleagueApi  = {
     pseudo: '',
     photo: '',
     first: '',
     last: '',
   };
+
+  constructor(
+    private colleagueService: ColleagueService
+  ) { }
   submit() {
-    console.log(this.colleague);
+    this.colleagueService.post(this.colleague).subscribe();
   }
 }
